60a3797fabdceb8990479a6cd592040bb9b054f3
JavaNoScriptt <56607571+JavaNoScriptt@users.noreply.github.com>
2024-05-19 20:00:22 -0500

    got a temp logo

--- text_based/main.py
+++ text_based/main.py
@@ -3,9 +3,7 @@ To-Do
 1. define functions, additon multiplacion verifiers etc.
 2. start on text_based
 3. get inputs
-4. 
-
-
+4. work on logo for calculator/personal logo
 
 
 """
@@ -14,19 +12,20 @@ To-Do
 
 
 def main():
-    print("""
-
-
-
-
-
-
+    #art made by chat gpt initally but might make a new one later on
+   print("""
+  ___      _      _          _       _            
+ / _ \    | |    | |        | |     | |           
+/ /_\ \ __| | ___| |__   ___| |_ ___| |_ ___  _ __ 
+|  _  |/ _` |/ _ \ '_ \ / _ \ __/ __| __/ _ \| '__|
+| | | | (_| |  __/ | | |  __/ |_\__ \ || (_) | |   
+\_| |_/\__,_|\___|_| |_|\___|\__|___/\__\___/|_|   
+         
 
 """)
-
-
-
-    return None
+   
+   
+   return None
 
 if __name__ == "__main__":
     main()

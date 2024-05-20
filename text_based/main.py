0a4bb3c179fcc375abbade842aa4efbf389fe4e8
JavaNoScriptt <56607571+JavaNoScriptt@users.noreply.github.com>
2024-05-19 19:43:38 -0500

    made project going to get text based functions working then go onto the graphical one

---
+++ text_based/main.py
@@ -0,0 +1,32 @@
+"""
+To-Do
+1. define functions, additon multiplacion verifiers etc.
+2. start on text_based
+3. get inputs
+4. 
+
+
+
+
+"""
+
+
+
+
+def main():
+    print("""
+
+
+
+
+
+
+
+""")
+
+
+
+    return None
+
+if __name__ == "__main__":
+    main()
